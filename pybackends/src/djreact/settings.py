e6b0190aab9750dcc9c5104ad135ab37bf1258ca
dougfoo <doug.cha@gmail.com>
2019-01-21 12:32:27 -0500

    added restframework and url

--- pybackends/src/djreact/settings.py
+++ pybackends/src/djreact/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'articles',
+    'rest_framework',
     'django.contrib.staticfiles',
 ]
 
@@ -118,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
