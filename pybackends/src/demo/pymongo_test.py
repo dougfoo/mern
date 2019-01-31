11d92ff03bcd083ad26410466cf150f97158fbca
dougfoo <doug.cha@gmail.com>
2019-01-31 01:35:20 -0500

    pip install issues

--- pybackends/src/demo/pymongo_test.py
+++ pybackends/src/demo/pymongo_test.py
@@ -1,11 +1,26 @@
-from pymongo import MongoClient
+import pymongo
+# version 2.8.1 !!??
 
 url = "mongodb://dougfooaccount:v9OCE0pGtEhPtRmZFiviZt7iLQovOVpuf5Vtr0ZzxHD3z0mcjTM9aoo31Jdf1ayW8smVT5hdY24CmHwYoSEhcg==@dougfooaccount.documents.azure.com:10255/Test?ssl=true&replicaSet=globaldb"
 
-client = MongoClient(url)
+client = pymongo.MongoClient(url)
 
 db = client.Test 
 col = db.T1
 
+print(db)
+print (col)
+
 import pprint
-pprint.pprint(col.find_one())
+for c in col.find():
+    pprint.pprint(c)
+
+c1 = {'id':'my id 3','content':'my lame content'}
+
+posts = db.posts
+post = posts.insert_one(c1)
+print(post)
+for c in posts.find():
+    pprint.pprint(c)
+
+    
